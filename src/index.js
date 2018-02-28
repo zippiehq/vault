38be6aeeb0979f42700f5a5998e8ed6509fd5da0
Carsten Munk <carsten.munk@zipperglobal.com>
2018-02-28 06:22:30 +0100

    Make buildable
    Signed-off-by: Carsten Munk <carsten.munk@zipperglobal.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ var HDKey = require('hdkey')
 var secp256k1 = require('secp256k1')
 var shajs = require('sha.js')
 var store = require('store')
+const crypto = require('crypto');
 
+// vault per-session state
 var inited = false
 var apphash = null
 var origin = null
 var pubex = null
+var pubex_hd = null
 
 // vault contains:
 // cache of app hash -> app-pubex
@@ -15,9 +18,8 @@ var pubex = null
 // seed piece 1 out of 2 (2of2) encrypted with device local private key
 // forgetme server url if non-standard
 
-// app-pubex is calculated by taking private extended key of root + some derivation, always hardened +
+// an app-pubex is calculated by taking private extended key of root + some derivation, always hardened +
 //   [for every 32 bit of the 256-bit hash, take the hardended child of index (value integer divided with 2^31) and then the hardened child of index (value integer mod 2^31)
-
 function deriveWithHash(hdkey, hash) {
   for (var i = 0; i < 32; i += 4) {
     var value = hash.readUInt32LE(i)
@@ -56,7 +58,7 @@ function vaultInit(event) {
       apphash = store.get('vault-cookie-' + magiccookie)
       store.remove('vault-cookie-' + magiccookie)
       if (apphash) {
-        pubex = store.get('pubex-' + apphash.toString('hex')){
+        pubex = store.get('pubex-' + apphash.toString('hex'))
         // redirection should have given a pubex already, else balk and send 'please re-launch' back
         if (pubex == null) {
           // balk and send 'please re-launch' back
@@ -70,57 +72,64 @@ function vaultInit(event) {
       }
     } else {
       parent.postMessage({'callback' : callback, 'result' : 'launch', launch : location.href, reason: 'not trusted nor cookie'}, event.origin)
-    }    
-}
-
-
-
-// vault is initially started with device local key which enables it to decrypt it's local pieces
-/* 
-var seed = 'a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af'
-var hdkey = HDKey.fromMasterSeed(new Buffer(seed, 'hex'))
-
-var hash = shajs('sha256').update('notshit').digest().toString('hex')
-
-console.log(deriveWithHash(hdkey, hash).publicExtendedKey)
-
-function handleVaultMessage(event) {
-  if (event.source == parent) {
-    if ('init' in event.data && !inited)
-    {
-      vaultInit(event);
-      inited = true
-      return;
     }
-
-    if (!inited)
-      return;
-    
   }
+  // okay, now we have apphash and pubex
+  pubex_hdkey = HDKey.fromExtendedKey(pubex)
+  inited = true
+  parent.postMessage({'callback' : callback, 'result' : 'inited'}, event.origin)
 }
-*/
 
-if (window.top == window.self) {
+async function getSeed() {
+  // in real case this gets the other slice from the server and grabs seed for a moment
+  return new Promise(resolve => {
+    // XXX FIXME
+    resolve(new Buffer('a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af', 'hex'))
+  })
+}
+
+async function setup() {
   // we either:
   // - launch a uri w/ a cookie for authentication towards an app-pubex
   // - start a signup process and afterwards launch a uri as linked
   // XXX don't bother to allow anything if not initialised, show up signup?
+  if (store.get('vaultSetup') == null) {
+    alert('vault not setup')
+  }
   
   if (location.hash.startsWith('#launch=')) {
-    // TODO: slice off the # in the end
+    // TODO: slice off the # in the end of target uri to allow deep returns but same context
     var uri = location.hash.slice('#launch='.length)
     apphash = shajs('sha256').update(uri).digest()
     pubex = store.get('pubex-' + apphash.toString('hex'))
-    if (pubex != null)
-    {
-      // we're ready to launch
-      // generate a one time cookie and redirect to the new uri + cookie
-    } else {
-      // we need to generate public extended key for this particular apphash
+    if (pubex == null) {
+       let seed = await getSeed()
+       var hdkey = HDKey.fromMasterSeed(seed)
+       pubex_hdkey = HDKey.fromExtendedKey(deriveWithHash(hdkey, apphash).publicExtendedKey)
+       pubex = pubex_hdkey.publicExtendedKey
+       store.set('pubex-' + apphash.toString('hex'), pubex)
     }
+    // we're now ready to launch
+    // generate a one time cookie and redirect to the new uri + cookie
+    crypto.randomBytes(32, (err, buf) => {
+      if (err) {
+        throw err
+      }
+      var vaultcookie = buf.toString('hex')
+      // XXX Should this be a session cookie or somehow related to apphash for cleaning out later?
+      store.set('vault-cookie-' + vaultcookie, apphash.toString('hex'))
+      // TODO: add deep return possible
+      window.location = uri + '#zipper-vault=' + location.href.split('#')[0] + '#' + vaultcookie
+    })
   } else if (location.hash.startsWith('#signup=')) {
-    
+    // show signup UX, generate keys
   }
+}
+
+if (window.top == window.self) {
+  setup().then(() => {
+    console.log('Setup done')
+  })
 } else {
   // we're in an iframe, time to listen for commands
   console.log('Zipper Vault listening')
