ed606737f152f9e9e4002f221b96da726734ac56
Tom Swindell <tom@zippie.org>
2018-05-14 14:13:12 +0100

    Added camera allow flag to iframe for Chrome.
    Signed-off-by: Tom Swindell <tom@zippie.org>

--- src/index.js
+++ src/index.js
@@ -447,6 +447,7 @@ async function setup() {
 
     var iframe = document.createElement('iframe')
     iframe.style.cssText = 'border: 0; position:fixed; top:0; left:0; right:0; bottom:0; width:100%; height:100%'
+    iframe.allow = 'camera'
     iframe.src = uri.split('#')[0] + '#iframe=' + vaultcookie
     document.body.appendChild(iframe)
     rootWindow = iframe.contentWindow
